5aa14e857c58a273227d3c0edc220fcc06816b78
phamminhquan12c1 <phamminhquan12c1@gmail.com>
2023-04-23 14:37:57 +0700

    first commit 66

--- index.js
+++ index.js
@@ -44,6 +44,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // nhúng middleware vào express
 app.use(express.json())
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin',process.env.client_url );
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 //sau khi chạy route nếu lỗi thì bắn ra lỗi
 
 app.use("/api/tour",routerTour),
