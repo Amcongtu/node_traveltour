a44b7b38de3d8bc309fe1fc70ea14bde6a090671
AmCongTu <phamminhquan12c1.com>
2023-05-14 20:47:19 +0700

    push14

--- Controllers/contact.js
+++ Controllers/contact.js
@@ -18,3 +18,30 @@ export const insertContact = async (req, res, next) => {
       next(err);
     }
 };
+
+export const getAllContact = async(req,res,next)=>{
+  try {
+    const contacts = await Contact.find()
+    res.status(200).json(contacts)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateContact = async(req,res,next)=>{
+  try {
+    const update = await Contact.findOneAndUpdate({_id: req.body._id},req.body,{new: true})
+    update ? res.status(200).json({success: true,data: update, message:'Update Success!'}): res.status(405).json({success: false, message: 'Some thing went worng!',data: null})
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deleteContact = async(req,res,next)=>{
+  try {
+    const deleteContact = await Contact.findOneAndDelete({_id: req.params.id})
+    deleteContact ? res.status(200).json({success: true,data: deleteContact, message:'Delete Success!'}): res.status(405).json({success: false, message: 'Some thing went worng!',data: null})
+  } catch (error) {
+    next(error)
+  }
+}
--- routes/routerReview.js
+++ routes/routerReview.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { deleteReview, getAllReviews, insertReview, updateReview } from "../Controllers/review.js";
 import { verifyClient } from "../utils/verifyToken.js";
+import { deleteContact, getAllContact, updateContact } from "../Controllers/contact.js";
 const router = express.Router()
 
 
@@ -8,5 +9,8 @@ router.post("/",insertReview)
 router.get('/',getAllReviews)
 router.delete('/:id',deleteReview)
 router.put('/:id',updateReview)
+router.get('/',getAllContact)
+router.put('/update',updateContact),
+router.delete('/:id',deleteContact)
 export default router
 
